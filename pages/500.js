bc6d82189ccf34397a6264fcece2c5fb984e420b
maelacudini <maelacudini@gmail.com>
2024-05-21 13:34:33 +0200

    first commit

--- pages/500.js
+++ pages/500.js
@@ -1,22 +1,12 @@
 import { useRouter } from "next/navigation"
 
-function Error({ statusCode }) {
+export default function Error() {
     let pathname = useRouter()
     return (
         <section className="error">
-            <p className="h2">
-                {statusCode
-                    ? `An error ${statusCode} occurred on server`
-                    : 'An error occurred on client'}
-            </p>
+            <p className="h2">An error occurred</p>
             <p onClick={() => pathname.refresh()} className="btn">Try again</p>
         </section>
     )
 }
 
-Error.getInitialProps = ({ res, err }) => {
-    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    return { statusCode }
-}
-
-export default Error
